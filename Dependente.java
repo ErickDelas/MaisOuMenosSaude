46748b07b8199d4cb464114aff64b7ef9893f452
Matthewbene03 <134011399+Matthewbene03@users.noreply.github.com>
2023-05-23 23:23:24 -0300

    Update Dependente.java

--- Dependente.java
+++ Dependente.java
@@ -1,9 +1,20 @@
 package maisoumenossaudetest;
 
+import java.util.ArrayList;
+
 public class Dependente extends Pessoa{
+    private ArrayList <Dependente> dependente = new ArrayList();
     
     public Dependente(String nome, String cpf, String endereco, 
         String nTelefone, String email) {
         super(nome, cpf, endereco, nTelefone, email);
+    }
+    
+    public void cadastrarDependente(Dependente d){
+        this.dependente.add(d);
+    }
+
+    public ArrayList<Dependente> getDependente() {
+        return dependente;
     }
 }
