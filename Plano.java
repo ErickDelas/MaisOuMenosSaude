f1b3ba05890bcc1b6a0024a4feac2b8bac6e7b23
Matthewbene03 <134011399+Matthewbene03@users.noreply.github.com>
2023-05-24 11:05:27 -0300

    Update Plano.java

--- Plano.java
+++ Plano.java
@@ -1,9 +1,12 @@
 package maisoumenossaudetest;
 
+import java.util.ArrayList;
+
 public class Plano {
     private double valorPlano;
     private String beneficiosPlano;
     //private String letrasMiudas; // O que seria isso, Arthur? está no diagrama de classes.
+    private ArrayList <Plano> plano = new ArrayList ();
     
     public Plano (double valorPlano, String beneficiosPlano){
         this.valorPlano = valorPlano;
@@ -11,6 +14,14 @@ public class Plano {
         //this.letrasMiudas = letrasMiudas;
     }
 
+    public void cadastrarPlano (Plano p){
+        this.plano.add(p);
+    }
+
+    public ArrayList<Plano> getPlano() {
+        return plano;
+    }
+    
     public double getValorPlano() {
         return valorPlano;
     }
