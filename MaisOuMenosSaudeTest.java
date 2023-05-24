a8f2afbb2096f56a00c756f9fe4b2e337e0f7cf6
Matthewbene03 <134011399+Matthewbene03@users.noreply.github.com>
2023-05-23 22:21:11 -0300

    Update MaisOuMenosSaudeTest.java

--- MaisOuMenosSaudeTest.java
+++ MaisOuMenosSaudeTest.java
@@ -1,8 +1,25 @@
 package maisoumenossaudetest;
 
+import java.util.Scanner;
+
 public class MaisOuMenosSaudeTest {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner (System.in);
+        int opcao = -1;
+        
+        while(opcao!=0){
+            Menu.exibeMenu();
+            opcao = sc.nextInt();
+            
+            if(opcao!=1 || opcao!=2 || opcao!=3 || opcao!= 4){
+                break;
+            } else if(opcao==0){
+                System.out.println("Você finalizou a sessão!");
+                break;
+            } else {                
+                Menu.executaAcao(opcao);
+            }
+        }
     }
 }
