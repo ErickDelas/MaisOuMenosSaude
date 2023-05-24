1c755bc13f9d837a0aee6a332d7a52f4392f3570
Matthewbene03 <134011399+Matthewbene03@users.noreply.github.com>
2023-05-23 23:25:11 -0300

    Update Atentende.java

--- Atentende.java
+++ Atentende.java
@@ -5,15 +5,21 @@ import java.time.LocalTime;
 
 public class Atentende extends Funcionario{
     private int totCancelamento;
+    private double comissaoFixa;
 
     public Atentende(String nome, String cpf, String endereco, String nTelefone, 
         String email, String nCtps, double salario, LocalDate dataContratacao, 
         LocalTime inicioExpediente, LocalTime fimExpediente, 
-        double ultimoContraCheque, double bonificacao, int totCancelamento) {
+        double ultimoContraCheque, double bonificacao, double comissaoFixa) {
         super(nome, cpf, endereco, nTelefone, email, nCtps, salario, 
             dataContratacao, inicioExpediente, fimExpediente, ultimoContraCheque, 
             bonificacao);
-        this.totCancelamento = totCancelamento;
+            this.comissaoFixa = comissaoFixa;
+    }
+    
+    @Override
+    public void cadastrarFuncionario(Funcionario f){
+        f.getFuncionario().add(f);
     }
     
     public void cancelaPlano(){
