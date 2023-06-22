a2f1730c51d76ccdcdd1c66eab0f9a32a4ae6328
Matthewbene03 <134011399+Matthewbene03@users.noreply.github.com>
2023-06-22 10:27:30 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,46 +1,85 @@
 package maisoumenossaude;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
 
-public class Funcionario extends Pessoa{
+public class Funcionario extends Pessoa {
+
     private String nCtps;
     private double salario;
     private LocalDate dataContratacao;
-    private LocalTime inicioExpediente;
-    private LocalTime fimExpediente;
     private double ultimoContraCheque;
-    private double bonificacao;
-    //private ArrayList <Funcionario> funcionario = new ArrayList();
-    
-    public Funcionario(){
-        
-    }
-    
-    public Funcionario(String nome, String cpf, String endereco, 
-        String nTelefone, String email, String nCtps, double salario, 
-        LocalDate dataContratacao, LocalTime inicioExpediente, 
-        LocalTime fimExpediente, double ultimoContraCheque, double bonificacao) {
-        super(nome, cpf, endereco, nTelefone, email);
-        this.nCtps = nCtps; 
-        this.salario = salario; 
-        this.dataContratacao = dataContratacao; 
-        this.inicioExpediente = inicioExpediente; 
-        this.fimExpediente = fimExpediente; 
-        this.ultimoContraCheque = ultimoContraCheque; 
-        this.bonificacao = bonificacao;
+    private double comissaoFixa;
+
+    public Funcionario() {
+
     }
 
-    public double calculaBonificacao(){
+    public Funcionario(String nome, String cpf, String endereco,
+            String nTelefone, String email, String nCtps, double salario,
+            LocalDate dataContratacao, double comissaoFixa) {
+        super(nome, cpf, endereco, nTelefone, email);
+        this.nCtps = nCtps;
+        this.salario = salario;
+        this.dataContratacao = dataContratacao;
+        this.comissaoFixa = comissaoFixa;
+        this.ultimoContraCheque = 0;
+    }
+
+    public double calculaBonificacao() {
         return 0;
     }
-    
-    public void recebeBonificacao(){
-        
+
+    public void recebeBonificacao() {
+
     }
-    
-    public void cadastrarFuncionario(Funcionario f){
-        
+
+
+    public String getnCtps() {
+        return nCtps;
+    }
+
+    public void setnCtps(String nCtps) {
+        this.nCtps = nCtps;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public LocalDate getDataContratacao() {
+        return dataContratacao;
+    }
+
+    public void setDataContratacao(LocalDate dataContratacao) {
+        this.dataContratacao = dataContratacao;
+    }
+
+    public double getUltimoContraCheque() {
+        return ultimoContraCheque;
+    }
+
+    public void setUltimoContraCheque(double ultimoContraCheque) {
+        this.ultimoContraCheque = ultimoContraCheque;
+    }
+
+    public double getComissaoFixa() {
+        return comissaoFixa;
+    }
+
+    public void setComissaoFixa(double comissaoFixa) {
+        this.comissaoFixa = comissaoFixa;
+    }
+
+    @Override
+    public String toString() {
+        super.toString();
+        return "Salario: "+this.salario
+                +"\nnCTPS: "+this.nCtps
+                +"\nData de contratação: ("+this.dataContratacao+")"
+                +"\nComissão Fixa: "+this.comissaoFixa;
     }
 }
