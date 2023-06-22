d43edc9a84dc9c74d5a471c9af0e269bfa8fccfb
Matthewbene03 <134011399+Matthewbene03@users.noreply.github.com>
2023-06-22 10:28:03 -0300

    Update Gerente.java

--- Gerente.java
+++ Gerente.java
@@ -1,61 +1,158 @@
 package maisoumenossaude;
 
+import cidades.Enum.Cidades;
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.Scanner;
 
-public class Gerente extends Funcionario{
-    private double comissaoFixa;
-    private BDados bd = new BDados();
-    private Credenciais c = new Credenciais();
-    
-    public Gerente(){
-        
-    }
-    
-    public Gerente(String nome, String cpf, String endereco, String nTelefone, 
-        String email, String nCtps, double salario, LocalDate dataContratacao, 
-        LocalTime inicioExpediente, LocalTime fimExpediente, 
-        double ultimoContraCheque, double bonificacao, double comissaoFixa,
-        String usuario, String senha) {
-        super(nome, cpf, endereco, nTelefone, email, nCtps, salario, 
-            dataContratacao, inicioExpediente, fimExpediente, ultimoContraCheque, 
-            bonificacao);
-        this.comissaoFixa = comissaoFixa;
-        c.setUsuario(usuario);
-        c.setSenha(senha);
-    }
-    
-    /*Recebe um vendedor como parametro e add em um ArrayList do tipo vendedor
-    que tem na classe BDados.
-    */
-    public void cadastrarFuncionario(Vendedor v){
-        this.bd.getVendedor().add(v);   
-    }
-    
-    /*Recebe um Atendente como parametro e add em um ArrayList do tipo Atendente
-    que tem na classe BDados.
-    */
-    public void cadastrarFuncionario(Atendente a){
-        this.bd.getAtendente().add(a);
-    }
-    
-    public void pagarFuncionario(Funcionario f){
-        
-    }
-    
-    public void calculaBonificacao1(){
-        
+public class Gerente extends Funcionario {
+
+    private Cidades cidade;
+    private Credenciais credenciaisGerente;
+
+    public Gerente() {
+
     }
 
-    public Credenciais getC() {
-        return c;
+    public Gerente(String nome, String cpf, String endereco, String nTelefone,
+            String email, String nCtps, double salario, LocalDate dataContratacao,
+            double comissaoFixa, Credenciais credenciaisGerente) {
+        super(nome, cpf, endereco, nTelefone, email, nCtps, salario,
+                dataContratacao, comissaoFixa);
+        this.credenciaisGerente = credenciaisGerente;
+    }
+
+    public void cadastrarVendedor(Cidades cidade) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+        System.out.print("CPF: ");
+        String cpf = sc.nextLine();
+        System.out.print("Endereço: ");
+        String endereco = sc.nextLine();
+        System.out.print("Numero de telefone: ");
+        String nTelefone = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        System.out.print("Numero da carteira de trabalho: ");
+        String nCtps = sc.nextLine();
+        System.out.print("Salario: ");
+        double salario = sc.nextDouble();
+        System.out.print("Comissao Fixa: ");
+        double comissaoF = sc.nextDouble();
+        System.out.println("Agora, informe um usuario e senha para acessar o sistema.");
+        System.out.print("Usuario: ");
+        sc.nextLine();
+        String usuario = sc.nextLine();
+        System.out.print("Senha: ");
+        String senha = sc.nextLine();
+        Vendedor vendedor = new Vendedor(nome, cpf, endereco, nTelefone, email,
+                nCtps, salario, LocalDate.now(), comissaoF,
+                new Credenciais(usuario, senha));
+        vendedor.setCidade(cidade);
+        BDados.getVendedores().add(vendedor);
+    }
+
+    public void cadastrarAtendente(Cidades cidade) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+        System.out.print("CPF: ");
+        String cpf = sc.nextLine();
+        System.out.print("Endereço: ");
+        String endereco = sc.nextLine();
+        System.out.print("Numero de telefone: ");
+        String nTelefone = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        System.out.print("Numero da carteira de trabalho: ");
+        String nCtps = sc.nextLine();
+        System.out.print("Salario: ");
+        double salario = sc.nextDouble();
+        System.out.print("Comissao Fixa: ");
+        double comissaoF = sc.nextDouble();
+        System.out.println("Agora, informe um usuario e senha para acessar o sistema.");
+        System.out.print("Usuario: ");
+        sc.nextLine();
+        String usuario = sc.nextLine();
+        System.out.print("Senha: ");
+        String senha = sc.nextLine();
+        Atendente atendente = new Atendente(nome, cpf, endereco, nTelefone, email,
+                nCtps, salario, LocalDate.now(), comissaoF,
+                new Credenciais(usuario, senha));
+        atendente.setCidade(cidade);
+        BDados.getAtendentes().add(atendente);
+    }
+
+    public void exibirAtendente() {
+        System.out.println("Lista de atendentes: ");
+        for (Atendente atendentes : BDados.getAtendentes()) {
+            System.out.println(atendentes);
+        }
+    }
+
+    public void exibirVendedor() {
+        System.out.println("Lista de vendedores: ");
+        for (Vendedor vendedores : BDados.getVendedores()) {
+            System.out.println(vendedores);
+        }
+    }
+
+    public void pagarVendedor(Vendedor v) {
+        Double bonificacao = calculaBonificacaoVendedor(v);
+        v.setUltimoContraCheque(bonificacao + v.getSalario());
+    }
+
+    public double calculaBonificacaoVendedor(Vendedor v) {
+        return v.getComissaoFixa() * v.getTotVendasMes();
+    }
+
+    public void pagarAtendente(Atendente a) {
+        Double bonificacao = calculaBonificacaoAtendente(a);
+        a.setUltimoContraCheque(bonificacao + a.getSalario());
+    }
+
+    public Double calculaBonificacaoAtendente(Atendente a) {
+        if(a.getComissaoFixa()>=100){
+            return a.getComissaoFixa() - (a.getTotCancelamento()*5);
+        } else {
+            return a.getComissaoFixa() - (a.getTotCancelamento() * 2);
+        }
+    }
+
+    public void atribuirPlanoVendedor(Vendedor v){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Qual o nome do plano irá ser atribuido ao vendedor " + v.getNome() + " ?");
+        String nomePlano = sc.nextLine().toUpperCase();
+        for(Plano p: BDados.getPlanos()){
+            if(nomePlano.equals(p.getNomePlano().toUpperCase())){
+                v.getListaPlanoVendedorTem().add(p);
+            }
+        }
     }
     
-    
+    public Credenciais getCredenciaisGerente() {
+        return credenciaisGerente;
+    }
+
+    public Cidades getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidades cidade) {
+        this.cidade = cidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + this.getNome()
+                + " - CPF: " + this.getCpf()
+                + " - Numero de telefone: " + this.getnTelefone()
+                + " - Email: " + this.getEmail()
+                + " - Endereço: " + this.getEndereco()
+                + " - Numero da carteira de trabalho: " + this.getnCtps()
+                + " - Salario: " + this.getSalario()
+                + " - Data de contratação: (" + this.getDataContratacao() + ")"
+                + " - Comissao Fixa: " + this.getComissaoFixa()
+                + " - Esse vendedor trabalha na cidade " + this.cidade;
+    }
 }
-/*
-Cada uma das três unidades da empresa possui um gerente, vendedores e atendentes. Um gerente é
-responsável por contratar/cadastrar os funcionários da sua unidade e também por informar o pagamento de
-cada um no sistema. Além disso, é o gerente quem atribui um plano de saúde para ser vendido por um
-vendedor de sua unidade
-*/
