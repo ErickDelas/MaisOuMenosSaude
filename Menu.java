8715a67738cbae7c939836b7b77a5f49b1a3108c
Matthewbene03 <134011399+Matthewbene03@users.noreply.github.com>
2023-06-22 22:11:08 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -9,7 +9,6 @@ public class Menu {
     public static void exibeMenuInicial() {
         System.out.println("\nSeja Bem-Vindo ao Mais Ou Menos Saúde.");
         System.out.println("[1] Fazer Login; ");
-        System.out.println("[2] Mostrar Lista de Funcionarios na empresa; ");
         System.out.println("[0] Sair;");
         System.out.print("Resposta: ");
     }
@@ -24,40 +23,47 @@ public class Menu {
         System.out.println("\nO que deseja fazer " + adm.getNomeAdm() + " : ");
         System.out.println("[1] Trocar usuario e senha do Administrador;");
         System.out.println("[2] Cadastrar Gerente;");
-        System.out.println("[3] Exibir lista de gerentes;");
-        System.out.println("[4] Cadastrar Planos;");
-        System.out.println("[5] Exibir lista de Planos;");
+        System.out.println("[3] Demitir Gerente;");
+        System.out.println("[4] Exibir lista de gerentes;");
+        System.out.println("[5] Cadastrar Planos;");
+        System.out.println("[6] Deletar Plano de Saúde;");
+        System.out.println("[7] Exibir lista de Planos;");
         System.out.println("[0] Sair;");
         System.out.print("Resposta: ");
+        //System.out.println("[11] Alterar Valores que os Funcionários recebem;");
     }
 
     public static void acoesGerente(Gerente gerente) {
         System.out.println("\nO que deseja fazer " + gerente.getNome() + " : ");
-        System.out.println("[1] Cadastrar Atendente;");
-        System.out.println("[2] Exibir lista de Atendentes;");
-        System.out.println("[3] Cadastrar Vendedor;");
-        System.out.println("[4] Exibir lista de Vendedores;");
-        System.out.println("[5] Atribuir um plano para um vendedor;");
-        System.out.println("[6] Pagar Atendente;");
-        System.out.println("[7] Pagar Vendedor");
+        System.out.println("[1] Contratar Atendente;");
+        System.out.println("[2] Demitir Atendente;");
+        System.out.println("[3] Exibir lista de Atendentes;");
+        System.out.println("[4] Contratar Vendedor;");
+        System.out.println("[5] Demitir Vendedor;");
+        System.out.println("[6] Exibir lista de Vendedores;");
+        System.out.println("[7] Atribuir um plano para um vendedor;");
+        System.out.println("[8] Pagar Atendente;");
+        System.out.println("[9] Pagar Vendedor");
         System.out.println("[0] Sair;");
         System.out.print("Resposta: ");
+
     }
 
     public static void acoesVendedor(Vendedor vendedor) {
         System.out.println("\nO que deseja fazer " + vendedor.getNome() + " : ");
         System.out.println("[1] Vender plano;");
+        System.out.println("[2] Exibir lista de Clientes Ativos");
         System.out.println("[0] Sair;");
         System.out.print("Resposta: ");
     }
 
     public static void acoesAtendente(Atendente atendente) {
         System.out.println("\nO que deseja fazer " + atendente.getNome() + " : ");
-        System.out.println("[1] Cadastrar Clientes;");
-        System.out.println("[2] Exibir lista de Clientes;");
+        System.out.println("[1] Cadastrar Possiveis Clientes;");
+        System.out.println("[2] Exibir lista de Possiveis Clientes;");
         System.out.println("[3] Cancelar um plano de um cliente;");
+        System.out.println("[4] Exibir lista de Clientes inativos");
         System.out.println("[0] Sair;");
         System.out.print("Resposta: ");
     }
-
 }
