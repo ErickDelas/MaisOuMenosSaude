e0a569dd8fb0e47e6a72a76c9df9c715e409cb56
Matthewbene03 <134011399+Matthewbene03@users.noreply.github.com>
2023-06-23 17:27:38 -0300

    Update Atendente.java

--- Atendente.java
+++ Atendente.java
@@ -23,48 +23,129 @@ public class Atendente extends Funcionario {
 
     public void cadastrarCliente() {
         Scanner sc = new Scanner(System.in);
+        boolean temAtributo = true;
+        String cpf = "";
+        String nTelefone = "";
+        String email = "";
 
         System.out.print("Nome: ");
         String nome = sc.nextLine();
-        System.out.print("CPF: ");
-        String cpf = sc.nextLine();
+        while (temAtributo == true) {
+            int aux = 0;
+            System.out.print("CPF: ");
+            cpf = sc.nextLine();
+            for (Cliente clientes : BDados.getPossiveisClientes()) {
+                if (clientes.getCpf().equals(cpf)) {
+                    System.out.println("ERROR! Ja tem um cliente cadastrado com esse CPF");
+                    System.out.println("Tente novamente!!!");
+                    aux++;
+                    break;
+                } else {
+                    temAtributo = false;
+                }
+                aux++;
+            }
+            if (aux == 0) {
+                temAtributo = false;
+            }
+        }
+        temAtributo = true;
         System.out.print("Endereço: ");
         String endereco = sc.nextLine();
-        System.out.print("Numero de telefone: ");
-        String nTelefone = sc.nextLine();
-        System.out.print("Email: ");
-        String email = sc.nextLine();
+        while (temAtributo == true) {
+            int aux = 0;
+            System.out.print("Numero de telefone: ");
+            nTelefone = sc.nextLine();
+            for (Cliente clientes : BDados.getPossiveisClientes()) {
+                if (clientes.getnTelefone().equals(nTelefone)) {
+                    System.out.println("ERROR! Ja tem um gerente cadastrado com esse Numero de telefone");
+                    System.out.println("Tente novamente!!!");
+                    aux++;
+                    break;
+                } else {
+                    temAtributo = false;
+                }
+                aux++;
+            }
+            if (aux == 0) {
+                temAtributo = false;
+            }
+        }
+        temAtributo = true;
+        while (temAtributo == true) {
+            int aux = 0;
+            System.out.print("Email: ");
+            email = sc.nextLine();
+            for (Cliente clientes : BDados.getPossiveisClientes()) {
+                if (clientes.getEmail().equals(email)) {
+                    System.out.println("ERROR! Ja tem um gerente cadastrado com esse email");
+                    System.out.println("Tente novamente!!!");
+                    aux++;
+                    break;
+                } else {
+                    temAtributo = false;
+                }
+                aux++;
+            }
+            if (aux == 0) {
+                temAtributo = false;
+            }
+        }
         Cliente cliente = new Cliente(nome, cpf, endereco, nTelefone, email);
         BDados.getPossiveisClientes().add(cliente);
     }
 
     public void exibirPossiveisClientes() {
+        boolean temCliente = false;
         System.out.println("Lista de possiveis clientes: ");
         for (Cliente clientesPossiveis : BDados.getPossiveisClientes()) {
             System.out.println(clientesPossiveis);
+            temCliente = true;
+        }
+        if (temCliente == false) {
+            System.out.println("Não tem nenhum cliente cadastrado!!!");
         }
     }
 
     public void exibirClientesInativos() {
+        boolean temCliente = false;
         System.out.println("Lista de clientes inativos: ");
-        for (Cliente clientesPossiveis : BDados.getClientesInativos()) {
-            System.out.println(clientesPossiveis);
+        for (Cliente clientesInativos : BDados.getClientesInativos()) {
+            System.out.println(clientesInativos);
+            temCliente = true;
+        }
+        if (temCliente == false) {
+            System.out.println("Não tem nenhum cliente inativo!!!");
         }
     }
 
     public void cancelaPlano() {
         Scanner sc = new Scanner(System.in);
-
+        boolean temCliente = false;
         System.out.print("Informe o CPF do cliente para cancelar o plano:");
         String cpfCliente = sc.nextLine().toUpperCase();
 
         for (Cliente c : BDados.getClientesAtivos()) {
             if (cpfCliente.equals(c.getCpf().toUpperCase())) {
+                c.setCpf(null);
+                c.setEmail(null);
+                c.setEndereco(null);
+                c.setnTelefone(null);
+                c.setDataHoraAquisicaoPlano(null);
+                c.setDependente(null);
+                c.setPlanoCliente(null);
+                c.setnCartaoCredito(null);
+                c.setnSus(null);
                 BDados.getClientesInativos().add(c);
                 BDados.getClientesAtivos().remove(c);
                 this.totCancelamento++;
+                System.out.println("O plano do cliente " + c.getNome() + " foi removido com sucesso!!!");
+                temCliente = true;
                 break;
             }
+        }
+        if (temCliente == false) {
+            System.out.println("Não tem nenhum cliente com o CPF " + cpfCliente + " que tenha um plano na nossa empresa!");
         }
     }
 
