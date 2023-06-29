60fadc75fee60c67dd51cfd01520caafc34a1e15
after001 <132841119+after001@users.noreply.github.com>
2023-06-28 21:44:36 -0300

    Update Dependente.java
    Organizado.

--- Dependente.java
+++ Dependente.java
@@ -1,14 +1,12 @@
-package maisoumenossaude;
-
+package application;
 
 public class Dependente extends Pessoa {
-    
-    public Dependente (){
-        
-    }
-    
-    public Dependente(String nome, String cpf, String endereco, 
-        String nTelefone, String email) {
-        super(nome, cpf, endereco, nTelefone, email);
-    }
+
+	public Dependente() {
+
+	}
+
+	public Dependente(String nome, String cpf, String endereco, String nTelefone, String email) {
+		super(nome, cpf, endereco, nTelefone, email);
+	}
 }
